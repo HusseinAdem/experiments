3f335fd9082d6dd1c2cb988c6f4e8551c477e256
Gazer174 <119759512+Gazer174@users.noreply.github.com>
2023-08-29 16:21:16 +0200

    Add files via upload

---
+++ StenSaxPase/src/Main.java
@@ -0,0 +1,95 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+        String[] arr={"sten", "sax", "påse"};
+        Random r=new Random();
+        String answer = "ja";
+
+        while (answer.equals("ja")) {
+
+        System.out.println("Sten, sax eller påse?: ");
+        String guess = input.nextLine();
+
+
+
+
+        int randomNumber=r.nextInt(arr.length);
+        String s = String.valueOf(randomNumber);
+        System.out.println(arr[randomNumber]);
+
+
+
+
+            if (guess.equals(arr[randomNumber])) {
+               System.out.println("Oavgjort!");
+               System.out.println("Vill du spela igen?");
+                answer = input.nextLine();
+
+           }
+
+            if (guess.equals(arr[0])) {
+                if (arr[randomNumber] == (arr[1])) {
+                    System.out.println("Du vann!");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+                }
+                if (arr[randomNumber] == (arr[2])) {
+                    System.out.println("Du förlorade");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+                }
+
+
+            }
+
+            if (guess.equals(arr[1])) {
+                if (arr[randomNumber] == (arr[2])) {
+                    System.out.println("Du vann!");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+                }
+                if (arr[randomNumber] == (arr[0])) {
+                    System.out.println("Du förlorade!");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+
+                }
+
+            }
+
+            if (guess.equals(arr[2])) {
+                if (arr[randomNumber] == (arr[0])) {
+                    System.out.println("Du vann!");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+
+                }
+                if (arr[randomNumber] == (arr[2])) {
+                    System.out.println("Du förlorade");
+                    System.out.println("Vill du spela igen?");
+                    answer = input.nextLine();
+
+                }
+
+            }
+        }
+        System.out.println("hejdå!");
+
+
+
+
+
+
+
+
+    }
+}
